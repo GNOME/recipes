3cfaee51809dda62b304849c5adf8cad56121f23
Matthias Clasen <mclasen@redhat.com>
2016-11-16 15:16:00 -0500

    Reload cuisines when recipes change

--- src/gr-cuisines-page.c
+++ src/gr-cuisines-page.c
@@ -39,6 +39,8 @@ struct _GrCuisinesPage
 
         GtkWidget *top_box;
         GtkWidget *flow_box;
+
+        char *featured;
 };
 
 G_DEFINE_TYPE (GrCuisinesPage, gr_cuisines_page, GTK_TYPE_BOX)
@@ -46,22 +48,23 @@ G_DEFINE_TYPE (GrCuisinesPage, gr_cuisines_page, GTK_TYPE_BOX)
 static void
 cuisines_page_finalize (GObject *object)
 {
+        GrCuisinesPage *page = GR_CUISINES_PAGE (object);
+
+        g_clear_pointer (&page->featured, g_free);
+
         G_OBJECT_CLASS (gr_cuisines_page_parent_class)->finalize (object);
 }
 
 static void
-gr_cuisines_page_init (GrCuisinesPage *page)
+populate_cuisines (GrCuisinesPage *page)
 {
         GtkWidget *tile;
         const char **all_cuisines;
         g_autofree char **cuisines = NULL;
         int length;
         int i, j;
-        int pos;
         GrRecipeStore *store;
-
-        gtk_widget_set_has_window (GTK_WIDGET (page), FALSE);
-        gtk_widget_init_template (GTK_WIDGET (page));
+        int pos;
 
         store = gr_app_get_recipe_store (GR_APP (g_application_get_default ()));
 
@@ -74,11 +77,16 @@ gr_cuisines_page_init (GrCuisinesPage *page)
                 cuisines[j++] = (char *)all_cuisines[i];
         }
 
+        if (page->featured && !g_strv_contains ((const char * const *)cuisines, page->featured))
+                g_clear_pointer (&page->featured, g_free);
+
         length = g_strv_length (cuisines);
+        if (!page->featured) {
+                pos = g_random_int_range (0, length);
+                page->featured = g_strdup (cuisines[pos]);
+        }
 
-        pos = g_random_int_range (0, length);
-
-        tile = gr_big_cuisine_tile_new (cuisines[pos]);
+        tile = gr_big_cuisine_tile_new (page->featured);
 	gtk_widget_show (tile);
         gtk_container_add (GTK_CONTAINER (page->top_box), tile);
 
@@ -90,6 +98,17 @@ gr_cuisines_page_init (GrCuisinesPage *page)
                 gtk_widget_show (tile);
                 gtk_container_add (GTK_CONTAINER (page->flow_box), tile);
         }
+}
+
+static void connect_store_signals (GrCuisinesPage *page);
+
+static void
+gr_cuisines_page_init (GrCuisinesPage *page)
+{
+        gtk_widget_set_has_window (GTK_WIDGET (page), FALSE);
+        gtk_widget_init_template (GTK_WIDGET (page));
+        populate_cuisines (page);
+        connect_store_signals (page);
 }
 
 static void
@@ -114,4 +133,26 @@ gr_cuisines_page_new (void)
         page = g_object_new (GR_TYPE_CUISINES_PAGE, NULL);
 
         return GTK_WIDGET (page);
+}
+
+static void
+cuisines_page_reload (GrCuisinesPage *page)
+{
+        container_remove_all (GTK_CONTAINER (page->top_box));
+        container_remove_all (GTK_CONTAINER (page->flow_box));
+
+        populate_cuisines (page);
+}
+
+static void
+connect_store_signals (GrCuisinesPage *page)
+{
+        GrRecipeStore *store;
+
+        store = gr_app_get_recipe_store (GR_APP (g_application_get_default ()));
+
+        /* FIXME: inefficient */
+        g_signal_connect_swapped (store, "recipe-added", G_CALLBACK (cuisines_page_reload), page);
+        g_signal_connect_swapped (store, "recipe-removed", G_CALLBACK (cuisines_page_reload), page);
+        g_signal_connect_swapped (store, "recipe-changed", G_CALLBACK (cuisines_page_reload), page);
 }
