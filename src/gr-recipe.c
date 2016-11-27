9bd82b5f9786b437d1c4e2d663d83449447c821c
Matthias Clasen <mclasen@redhat.com>
2016-11-27 18:03:30 -0500

    Don't make ctime construct-only
    This leads to critical warnings when saving new recipes, and is
    not really necessary.

--- src/gr-recipe.c
+++ src/gr-recipe.c
@@ -384,7 +384,7 @@ gr_recipe_class_init (GrRecipeClass *klass)
 
         pspec = g_param_spec_boxed ("ctime", NULL, NULL,
                                     G_TYPE_DATE_TIME,
-                                    G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY);
+                                    G_PARAM_READWRITE);
         g_object_class_install_property (object_class, PROP_CTIME, pspec);
 
         pspec = g_param_spec_boxed ("mtime", NULL, NULL,
