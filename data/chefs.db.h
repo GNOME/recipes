21f10936707dfe10bf09492477b63fd3119cc6e2
Matthias Clasen <mclasen@redhat.com>
2017-01-18 06:01:38 -0500

    Regenerate translatable recipe content

--- data/chefs.db.h
+++ data/chefs.db.h
@@ -1,5 +1,6 @@
 #if 0
 
+N_("I cook a lot, particularly Italian and Indian.")
 N_("The nordic cuisine maestro.")
 N_("Ray is a Desktop Software Engineer at Red Hat and vegetarian who occasionally enjoys cooking.")
 N_("My name is Fredrik Fyksen. I am from Norway, but live in Denmark. I like food, computers and music. You can read more about me at <a href=\"https://www.fyksen.me/\">fyksen.me</a>.")
