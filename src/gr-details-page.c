ddb1a52e7c4999bb765bb83fa6d9c186a726b128
Matthias Clasen <mclasen@redhat.com>
2017-02-28 22:03:37 -0500

    details page: Use less fancy glyphs
    The glyphs we were using for timers and images don't display
    on OS X (at least with a pango thats built by jhbuild), so
    lets use simpler ones that work.

--- src/gr-details-page.c
+++ src/gr-details-page.c
@@ -583,12 +583,12 @@ process_instructions (const char *instructions)
                         g_string_append_printf (s, _("Timer: %s"), str);
                         if (step->title)
                                 g_string_append_printf (s, "\n%s", step->title);
-                        g_string_append (s, "\">⏰</a>");
+                        g_string_append (s, "\">♢</a>");
                 }
                 else if (step->image != -1) {
                         g_string_append_printf (s, "<a href=\"image:%d\" title=\"", step->image);
                         g_string_append_printf (s, _("Image %d"), step->image + 1);
-                        g_string_append (s, "\">👁</a>");
+                        g_string_append (s, "\">♦</a>");
                 }
 
                 g_string_append (s, step->text);
