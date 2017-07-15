8f323950ff119411430a6ecd0a4867724a0c7c72
Piotr Drąg <piotrdrag@gmail.com>
2017-07-15 03:34:24 +0200

    Quick typography touch-ups

--- src/gr-window.c
+++ src/gr-window.c
@@ -1601,7 +1601,7 @@ play_tada_sound (GtkWindow *win)
         ca_context_play (c, 0,
                          CA_PROP_MEDIA_ROLE, "alert",
                          CA_PROP_MEDIA_FILENAME, path,
-                         CA_PROP_MEDIA_NAME, _("GNOME turns 20 !"),
+                         CA_PROP_MEDIA_NAME, _("GNOME turns 20!"),
                          NULL);
 #endif
 }
